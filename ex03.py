ba54664a908692e79bba19fb8f62bcca5c46b8b3
Jessica Silva <jessica.akyla@gmail.com>
2023-10-13 20:18:26 -0300

    Exercício com while

---
+++ ex03.py
@@ -0,0 +1,25 @@
+# PRATICANDO COM WHILE - LOOP
+
+# Imprima na tela os números de 0 a 10
+
+# Variável com o valor de inicio
+
+i = 1
+
+# Enquanto (i) for menor que 10, exiba o valor de (i) até chegar no 10
+
+# Uma atenção se quero até 10 então coloco 11, ou então coloco 10 + 1 ai chega no valor desejado.
+
+while i < 10 + 1:
+    print(i)
+    
+# se rodar o código assim, ele vai ter um loop infinito de 1 1 1 1 1...
+
+# Então vou acresecentar + 1 ao meu (i) assim quando verificar que 1 é menor que 10, vai ser somado + 1 ao valor de (i) que se torna 2, e assim continua o processo até chegar no valor desejado.
+
+    i = i + 1
+
+# Ou i += 1
+
+
+
