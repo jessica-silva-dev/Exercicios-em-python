361e8a0b6f948b315f68de6a20b9cb6c1a681134
Jessica Silva <jessica.akyla@gmail.com>
2023-10-17 19:45:55 -0300

    Usando operadores aritméticos

--- ex05.py
+++ ex05.py
@@ -2,7 +2,9 @@
 
 # informação para o usuário
 
-print('-------> PRIMEIRO EXERCÍCIO <-------')
+print('    -------> PRIMEIRO EXERCÍCIO <-------')
+print("Vou lhe mostrar qual é o antecessor e o sucessor do número desejado")
+print("vamos lá!")
 
 # Laço de repetição
 for i in range(0, 3):
@@ -18,10 +20,13 @@ for i in range(0, 3):
     print(f'Seu número é = {num}')
     print(f'O antecessor é = {antecessor}')
     print(f'O sucessor é = {sucessor}') 
+    print("Fácil né!?")
 
 # Informação para o usuário
 
-print('-------> PROXÍMO EXERCÍCIO <-------')
+print('     -------> PRÓXiMO EXERCÍCIO <-------')
+print('Agora você vai saber qual é o dobro o triplo e a raiz quadrada do número desejado')
+print("Preparando....")
     
 # Faça um programa que leia um número, e que mostre na tela qual é o dobro, o triplo e a raiz quadrado desse número.
 
@@ -31,8 +36,15 @@ for resultado in range(0, 3):
     num1 = int(input('Digite um número: '))
     dobro = num1 + num1
     triplo = num1 * 3
-    raiz = num1 **(1/2)
+    raiz = num1 **(0.5)
     print(f'O valor escolhido foi: {num1}')
     print(f'O dobro de {num1} é: {dobro}')
     print(f'O triplo de {num1} é:  {triplo}')
     print(f'A raiz quadrada de {num1} é : {raiz}')
+    
+    print("-------FIM--------")
+
+
+
+
+    
