7298b672e03587dac55637f22c4becc9ecdda11c
Jessica Silva <jessica.akyla@gmail.com>
2023-10-24 15:55:24 -0300

    Tabuada usando o for

---
+++ ex08.py
@@ -0,0 +1,29 @@
+# TABUADA
+
+# O usuario digita um número, e aparece a tabuada daquele número.
+
+for i in range(10): 
+    valor = int(input("Digite um número: "))
+    x1 = valor * 1
+    x2 = valor * 2
+    x3 = valor * 3
+    x4 = valor * 4
+    x5 = valor * 5
+    x6 = valor * 6
+    x7 = valor * 7
+    x8 = valor * 8
+    x9 = valor * 9
+    x10 = valor * 10
+
+    print(f"A tabuada de {valor} é: ")
+    print(f"{valor} x 1 = {x1}")
+    print(f"{valor} x 2 = {x2}")
+    print(f"{valor} x 3 = {x3}")
+    print(f"{valor} x 4 = {x4}")
+    print(f"{valor} x 5 = {x5}")
+    print(f"{valor} x 6 = {x6}")
+    print(f"{valor} x 7 = {x7}")
+    print(f"{valor} x 8 = {x8}")
+    print(f"{valor} x 9 = {x9}")
+    print(f"{valor} x 10 = {x10}")
+
