df38ab640f0d1bfc245d21690534f0c51b76c212
Jessica Silva <jessica.akyla@gmail.com>
2023-10-26 06:31:32 -0300

    Reajuste salarial

--- ex014.py
+++
@@ -1,9 +0,0 @@
-# Reajuste salarial
-
-salario = float(input("Qual é o valor do seu salário? R$"))
-desc = int(input("Porcentagem do reajuste salarial recebido: "))
-total = salario + (salario*desc/100)
-
-print(f"Seu sálario era de: R${salario}")
-print(f"Agora com o reajuste recebido de {desc}%")
-print(f"Parabéns! Você passa a receber: R${total:.2f}")
