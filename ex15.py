68594ef9ab746ad87c2133143c92de4f126e8c88
Jessica Silva <jessica.akyla@gmail.com>
2023-12-07 10:55:23 -0300

    Conversor de temperaturas de °C para °F.

---
+++ ex15.py
@@ -0,0 +1,7 @@
+# Conversor de temperaturas
+# Escreva um progrma que converta uma temperatura digitada em C° para °F
+
+temperatura_c = float(input("Informe a temperatura em °C"))
+temperatura_f = 9 * temperatura_c / 5 + 32
+
+print(f"A temperatura em °C é: {temperatura_c}\nConvertida em °F é: {temperatura_f}")
